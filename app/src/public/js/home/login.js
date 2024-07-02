197da7490993ab63daa51ad367caac9562c69ac7
“SeongHo.Yang” <yanglim@naver.com>
2024-07-03 02:46:11 +0900

    document

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("login.js 실행");
+// const { login } = require("../../../routes/home/home.ctrl");
+
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value
+    }
+    console.log(req);
+}
